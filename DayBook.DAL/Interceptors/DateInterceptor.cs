3650d829ad45a47388bfa5c575c41ac8fc5aa23b
madoha <madoha00@gmail.com>
2024-04-09 11:03:11 +0500

    Async repository

--- DayBook.DAL/Interceptors/DateInterceptor.cs
+++ DayBook.DAL/Interceptors/DateInterceptor.cs
@@ -6,7 +6,30 @@ namespace DayBook.DAL.Interceptors;
 
 public class DateInterceptor : SaveChangesInterceptor
 {
-    //public override ValueTask<int> SavedChangesAsync(SaveChangesCompletedEventData eventData, int result, CancellationToken cancellationToken = default)
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+    {
+        var dbContext = eventData.Context;
+        if (dbContext == null)
+        {
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+        var entries = dbContext.ChangeTracker.Entries<IAuditable>().Where(x => x.State == EntityState.Added || x.State == EntityState.Modified).ToList();
+        foreach (var entry in entries)
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Property(x => x.CreatedAt).CurrentValue = DateTime.UtcNow;
+            }
+            if (entry.State == EntityState.Modified)
+            {
+                entry.Property(x => x.UpdatedAt).CurrentValue = DateTime.UtcNow;
+            }
+        }
+
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+    //public override ValueTask<int> SavingChangesAsync(SaveChangesCompletedEventData eventData, int result, CancellationToken cancellationToken = default)
     //{
     //    var dbContext = eventData.Context;
     //    if (dbContext == null)
@@ -29,27 +52,27 @@ public class DateInterceptor : SaveChangesInterceptor
 
     //    return base.SavedChangesAsync(eventData, result, cancellationToken);
     //}
-    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
-    {
-        var dbContext = eventData.Context;
-        if (dbContext == null ) 
-        {
-            return base.SavingChanges(eventData, result);
-        }
+    //public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    //{
+    //    var dbContext = eventData.Context;
+    //    if (dbContext == null ) 
+    //    {
+    //        return base.SavingChanges(eventData, result);
+    //    }
 
-        var entries = dbContext.ChangeTracker.Entries<IAuditable>().Where(x => x.State == EntityState.Added || x.State == EntityState.Modified).ToList();
-        foreach (var entry in entries)
-        {
-            if (entry.State == EntityState.Added)
-            {
-                entry.Property(x => x.CreatedAt).CurrentValue = DateTime.UtcNow;
-            }
-            if(entry.State == EntityState.Modified)
-            {
-                entry.Property(x => x.UpdatedAt).CurrentValue = DateTime.UtcNow;
-            }
-        }
+    //    var entries = dbContext.ChangeTracker.Entries<IAuditable>().Where(x => x.State == EntityState.Added || x.State == EntityState.Modified).ToList();
+    //    foreach (var entry in entries)
+    //    {
+    //        if (entry.State == EntityState.Added)
+    //        {
+    //            entry.Property(x => x.CreatedAt).CurrentValue = DateTime.UtcNow;
+    //        }
+    //        if(entry.State == EntityState.Modified)
+    //        {
+    //            entry.Property(x => x.UpdatedAt).CurrentValue = DateTime.UtcNow;
+    //        }
+    //    }
 
-        return base.SavingChanges(eventData, result);
-    }
+    //    return base.SavingChanges(eventData, result);
+    //}
 }
